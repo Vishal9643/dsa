8448d18ffc824c49b21ad734f132cd4222ded182
vishal9643 <amitkrmishra864@gmail.com>
2022-09-19 03:44:39 +0530

    modified:   bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,16 +1,33 @@
-// let arr = [5, 7, 2, 6, 9, 4];
+let arr = [5, 7, 2, 6, 9, 4];
+console.log(arr);
+// const bubbleSort = (arr) => {
+// for (i = arr.length-1; i >= 0; i--) {
+//     for (j = arr.length-1; j >=0; j--) {
+//         if (arr[i] > arr[j]) {
+//             let temp = arr[j];
+//             arr[j] = arr[i];
+//             arr[i] = temp;
+//             console.log(arr);
+//         }
+//     }
+// }
+// console.log(arr);
+// }
+
+
 const bubbleSort = (arr) => {
-for (i = arr.length-1; i >= 0; i--) {
-    for (j = arr.length-1; j >=0; j--) {
-        if (arr[i] > arr[j]) {
-            let temp = arr[j];
-            arr[j] = arr[i];
-            arr[i] = temp;
+    for (i = 0; i < arr.length; i++) {
+        for (j = 0; j < arr.length; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                console.log(arr);
+            }
         }
     }
-}
-console.log(arr);
+    console.log(arr);
 }
 
-// bubbleSort(arr);
 
+bubbleSort(arr);
